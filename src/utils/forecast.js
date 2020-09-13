f6f0410d02e483f7fafccf9bbd5f1dc9857b6e0e
Saurabh Verma <saurabh.verma@auditoria.ai>
2020-09-13 10:00:18 +0530

    fix api

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require('request')
-
+const ACCESS_KEY = '9549ac0a93c9bb90c6129b75df278b42';
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/9549ac0a93c9bb90c6129b75df278b42/' + latitude + ',' + longitude
+    const url = `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${latitude},${longitude}&units=m`
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
-        } else if (body.error) {
+        } else if (!body.current) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.current.weather_descriptions + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.')
         }
     })
 }
